99aab0b8ba2edcaa54736bfcdd41d3bbe3bef48f
Andrew Heinke <andrew.j.heinke@gmail.com>
2019-01-23 23:26:05 -0600

    fix

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 import Container from './container'
-import './Footer.scss'
+import './footer.scss'
 
 const Footer = ({ children }) => (
   <footer>
