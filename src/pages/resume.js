4c39914f241e1d83bb671054d0a1ff0ee3a2eb23
Andrew Heinke <andrew.j.heinke@gmail.com>
2019-01-29 01:05:36 -0600

    feature: resume page complete

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import FluidLayout from '../components/fluid-layout'
 import SEO from '../components/seo'
-import { Row, Col } from 'react-grid-system'
+import { setConfiguration, Row, Col } from 'react-grid-system'
 import './resume.scss';
 
+setConfiguration({ gutterWidth: 60 });
 const ResumePage = () => (
   <FluidLayout>
     <SEO title="Resume Andrew Heinke" keywords={[`resume`, `andrew`, `heinke`]} />
@@ -20,151 +21,125 @@ A creative with coding skills, I create things that look good.</h2>
 	</div>
   <div className="bg-wrapper">
   <div className="container-0">
-		<ol className="timeline">
-		  <li>
-		    <h3 className="line">Work Experience</h3>
-		    <span className="point"></span>
-		    <p className="description">
-		      UI Developer <a href="https://www.usaa.com/careers/design" target="_blank" rel="noopener noreferrer">@USAA</a>
-		    </p>
-		    <span className="date">Today - Apr. 2017</span>
-		  </li>
-
-		  <li>
-		    <span className="point"></span>
-		    <p className="description">
-		      Web Designer <a href="https://kasasa.com/" target="_blank" rel="noopener noreferrer">@Kasasa</a>
-		    </p>
-		    <span className="date">Apr. 2017 - Oct. 2015</span>
-		  </li>
-
-		  		  <li>
-		    <h3 className="line">Education</h3>
-		    <span className="point"></span>
-		    <p className="description">
-		      UT Austin Coding Bootcamp
-		    </p>
-		    <span className="date">2016</span>
-		  </li>
-
-		  		  <li>
-		    <span className="point"></span>
-		    <p className="description">
-		      The University of Texas at Austin
-		    </p>
-		    <span className="date">2015 - 2011</span>
-		  </li>
-		</ol>
+  <Row>
+    <Col md={6}>
+    <div className="job-wrapper">
+		  <h3 className="line">Work Experience</h3>
+      <h4 className="job">UI Developer at <a className="company" href="https://www.usaa.com/careers/design" target="_blank" rel="noopener noreferrer">USAA</a></h4>
+      <p className="time">Apr 2017 - Present</p>
+      <p>Building accessible, reusable ReactJS components as part of design language team to create a unified brand standard across digital spaces. Updating internal and consumer facing web applications with NodeJS, ReactJS, Redux. Integrating third party APIs with web applications to simplify workflows and automate tasks.</p>
+      </div>
+      <div className="job-wrapper">
+        <h4 className="job">Web Designer at <a className="company" href="https://kasasa.com/" target="_blank" rel="noopener noreferrer">Kasasa</a></h4>
+        <p className="time">Oct 2015 - Apr 2017</p>
+        <p>Support for over 400 live websites for Community Financial Institutions. I design homepages, landing pages, banners & UI elements. I specialize in user experience and interaction. Through development of my coding knowledge I am able to design better for the web.</p>
+        </div>
+        <div className="job-wrapper">
+          <h4 className="job">Junior Developer at <a className="company" href="http://mosak.com/" target="_blank" rel="noopener noreferrer">MOSAK</a></h4>
+          <p className="time">Jan 2015 - Oct 2015</p>
+          <p>Email Marketing Manager - Building email campaigns, database selection, HTML development and quality assurance. Campaign set-up, testing, tagging, deployment of interactive content-driven campaigns, and compiling monthly email campaign analytics reports.</p>
+          <p>Web - Website updates/maintenance, development, and design. Optimize sites using SEO best practices. Compile monthly Google Analytics reports.</p>
+        <p>Skills Utilized: Blue Hornet (Email Marketing Software), HTML, CSS, Adobe Creative Suite, Wordpress, Sublime Text, Transmit FTP, Google Analytics & Web Master Tools</p>
+          </div>
+    </Col>
+    <Col md={6}>
+		<h3 className="line">Education</h3>
+    <div className="job-wrapper">
+      <h4 className="job">UT Coding Bootcamp</h4>
+      <p className="time">2016 - University of Texas at Austin</p>
+      <p>Browser Based Technologies: HTML, CSS, Git, JavaScript, React, jQuery, Responsive Design</p>
+      <p>Server Side Development: Node.js/Express, PHP (Wordpress, Codelgniter, Laravel), The LAMP Stack</p>
+      <p>Database: MySQL, MongoDB, Servers, Heroku, Security and Session Storage, User Authentication</p>
+      <p>Other Skills: Git/GitHub, HTML5, Bootstrap, PHP, Sessions, State Management, Sequelize, AJAX, Lodash, Backbone, RESTful API, JSON, Firebase</p>
+      </div>
+      <div className="job-wrapper">
+        <h4 className="job">B.S. Advertising</h4>
+        <p className="time">2011 - 2015 at The University of Texas at Austin</p>
+        <p>Browser Based Technologies: HTML, CSS, Git, JavaScript, React, jQuery, Responsive Design</p>
+        <p>Server Side Development: Node.js/Express, PHP (Wordpress, Codelgniter, Laravel), The LAMP Stack</p>
+        <p>Database: MySQL, MongoDB, Servers, Heroku, Security and Session Storage, User Authentication</p>
+        <p>Other Skills: Git/GitHub, HTML5, Bootstrap, PHP, Sessions, State Management, Sequelize, AJAX, Lodash, Backbone, RESTful API, JSON, Firebase</p>
+        </div>
+    </Col>
+    </Row>
 	</div>
   </div>
 	<div className="container">
-
+  <h3 class="line">Interests</h3>
   <Row>
+
     <Col md={6}>
-      <h2 className="section-title">Skills</h2>
 
-      <div className="list-card">
-        <span className="exp">+ 8 years</span>
-        <div>
-          <h3>Design & Design Thinking</h3>
-          <span>Using design to solve problems and deliver value</span>
-        </div>
-      </div>
-
-      <div className="list-card">
-        <span className="exp">+ 4 years</span>
-        <div>
-          <h3>Coding Languages</h3>
-          <span>HTML, CSS, JavaScript, React.js, Node.js </span>
-        </div>
-      </div>
-
-      <div className="list-card">
-        <span className="exp">+ 1 years</span>
-        <div>
-          <h3>Karaoke Singer</h3>
-          <span>Known to perform classic hits like Soulja Boy's "Kiss Me Thru the Phone"</span>
-        </div>
-      </div>
-    </Col>
-    <Col md={6}>
-      <h2 className="section-title">Interests</h2>
 
       <div className="list-card">
         <div>
           <h3>Painting</h3>
-          <span><a href="https://www.youtube.com/watch?v=U1-EdDAofqo" target="_blank" rel="noopener noreferrer">Watch me paint</a> in front of a camera (super cool)</span>
+          <span><a href="https://www.youtube.com/watch?v=U1-EdDAofqo" target="_blank" rel="noopener noreferrer" rel="noopener noreferrer">Watch me paint</a> in front of a camera (super cool)</span>
         </div>
       </div>
-
+      </Col>
+        <Col md={6}>
       <div className="list-card">
         <div>
           <h3>Hiking</h3>
-          <span><a href="https://www.insidehimalayas.com/wp-content/uploads/2018/07/3local.jpg" target="_blank" rel="noopener noreferrer">Here</a>, <a href="https://cdn.fodors.com/wp-content/uploads/2018/09/HERo_MachuPicchu101_Hero_4_MachuPicchu101_WhereCanIGetBestPhoto_dreamstime_xxl_110067560_2_1.jpg" target="_blank" rel="noopener noreferrer">Here</a>, and <a href="https://www.nationalgeographic.com/content/dam/expeditions/destinations/north-america/private/Yosemite/Hero-Yosemite.jpg" target="_blank" rel="noopener noreferrer">Here</a></span>
+          <span><a href="https://www.insidehimalayas.com/wp-content/uploads/2018/07/3local.jpg" target="_blank" rel="noopener noreferrer" rel="noopener noreferrer">Here</a>, <a href="https://cdn.fodors.com/wp-content/uploads/2018/09/HERo_MachuPicchu101_Hero_4_MachuPicchu101_WhereCanIGetBestPhoto_dreamstime_xxl_110067560_2_1.jpg" target="_blank" rel="noopener noreferrer" rel="noopener noreferrer">Here</a>, and <a href="https://www.nationalgeographic.com/content/dam/expeditions/destinations/north-america/private/Yosemite/Hero-Yosemite.jpg" target="_blank" rel="noopener noreferrer" rel="noopener noreferrer">Here</a></span>
         </div>
       </div>
     </Col>
   </Row>
 	</div>
 
-	<div className="container cards">
+	<div className="skills-container">
+  <div className="container">
+  <h2 className="skills-title">Skills</h2>
   <Row>
-    <Col md={6}>
-		<div className="card">
-			<div className="skill-level">
-				<span>+</span>
-				<h2>60</h2>
-			</div>
+    <Col md={12}>
+    <div className="skill-bar-container">
+      <span className="skill-name">HTML5 & CSS</span>
+      <span className="skill-percent">100%</span>
+      <span className="skill-bar level5"></span>
+    </div>
 
-			<div className="skill-meta">
-				<h3>Projects</h3>
-				<span>Adapting and creating solutions for customer's needs</span>
-			</div>
-		</div>
+    <div className="skill-bar-container">
+      <span className="skill-name">JavaScript</span>
+      <span className="skill-percent">90%</span>
+      <span className="skill-bar level4"></span>
+    </div>
 
-    </Col>
-      <Col md={6}>
-		<div className="card">
-			<div className="skill-level">
-				<h2>50</h2>
-				<span>%</span>
-			</div>
+    <div className="skill-bar-container">
+      <span className="skill-name">React</span>
+      <span className="skill-percent">85%</span>
+      <span className="skill-bar level3"></span>
+    </div>
 
-			<div className="skill-meta">
-				<h3>Web</h3>
-				<span>Applications development integrating third-party services and mobile client(s)</span>
-			</div>
-		</div>
-    </Col>
-    <Col md={6}>
-		<div className="card">
-			<div className="skill-level">
-				<h2>30</h2>
-				<span>%</span>
-			</div>
+    <div className="skill-bar-container">
+      <span className="skill-name">Node.js</span>
+      <span className="skill-percent">80%</span>
+      <span className="skill-bar level2"></span>
+    </div>
 
-			<div className="skill-meta">
-				<h3>Technical solutions</h3>
-				<span>Such as web services, scripts, configurations</span>
-			</div>
-		</div>
-    </Col>
-<Col md={6}>
-		<div className="card">
-			<div className="skill-level">
-				<h2>20</h2>
-				<span>%</span>
-			</div>
+    <div className="skill-bar-container">
+      <span className="skill-name">Design</span>
+      <span className="skill-percent">100%</span>
+      <span className="skill-bar level5"></span>
+    </div>
 
-			<div className="skill-meta">
-				<h3>Leading</h3>
-				<span>Web projects and ensure the quality of delivery</span>
-			</div>
-		</div>
+    <div className="skill-bar-container">
+      <span className="skill-name">Singing</span>
+      <span className="skill-percent">20%</span>
+      <span className="skill-bar level1"></span>
+    </div>
+
+    <div className="skill-bar-container">
+      <span className="skill-name">Adobe Creative Suite</span>
+      <span className="skill-percent">85%</span>
+      <span className="skill-bar level3"></span>
+    </div>
+
     </Col>
     </Row>
-	</div>
-
+    </div>
+    </div>
 
   </div>
   </FluidLayout>
